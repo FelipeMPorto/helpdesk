2e55fa3a9fde0d1d05f73024185d2a51dd89736a
FelipeMPorto <felipe423porto@gmail.com>
2022-03-01 12:14:26 -0300

    anotaçoes JPA para classes de dominio

--- src/main/java/com/felipe/helpdesk/domain/Cliente.java
+++ src/main/java/com/felipe/helpdesk/domain/Cliente.java
@@ -3,28 +3,35 @@ package com.felipe.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.felipe.helpdesk.domain.enums.Perfil;
+
+@Entity
 public class Cliente extends Pessoa {
+	private static final long serialVersionUID = 1L;
+	
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Chamado> chamados = new ArrayList<>();
 
-private List<Chamado> chamados = new ArrayList<>();
+	public Cliente() {
+		super();
+		addPerfil(Perfil.CLIENTE);
+	}
 
-public Cliente() {
-	super();
+	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+
 }
-
-public Cliente(Integer id, String nome, String cpf, String email, String senha) {
-	super(id, nome, cpf, email, senha);
-}
-
-public List<Chamado> getChamados() {
-	return chamados;
-}
-
-public void setChamados(List<Chamado> chamados) {
-	this.chamados = chamados;
-}
-
-
-
-}
-
-
--- src/main/java/com/felipe/helpdesk/domain/Tecnico.java
+++ src/main/java/com/felipe/helpdesk/domain/Tecnico.java
@@ -3,18 +3,27 @@ package com.felipe.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tecnico  extends Pessoa {
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
-	
-	
+import com.felipe.helpdesk.domain.enums.Perfil;
+
+@Entity
+public class Tecnico extends Pessoa {
+
+	private static final long serialVersionUID = 1L;
+
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfil(Perfil.TECNICO);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.TECNICO);
 	}
 
 	public List<Chamado> getChamados() {
@@ -24,7 +33,5 @@ public class Tecnico  extends Pessoa {
 	public void setChamados(List<Chamado> chamados) {
 		this.chamados = chamados;
 	}
-	
-	
-	
+
 }
